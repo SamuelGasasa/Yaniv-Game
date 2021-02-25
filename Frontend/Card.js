e09161c9911540b641cabb3b2086fa196892d669
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-02-25 13:03:41 +0200

    create class card

---
+++ Frontend/Card.js
@@ -0,0 +1,7 @@
+class card {
+  constructor(suit, rank, isJoker) {
+    this.suit = suit;
+    this.rank = rank;
+    this.isJoker = isJoker;
+  }
+}
---
+++ Frontend/script.js
@@ -0,0 +1,7 @@
+const deck = document.getElementById("deck");
+
+deck.addEventListener("onclick", dealDeck());
+
+function dealDeck() {
+  let currentDeck = 54;
+}
