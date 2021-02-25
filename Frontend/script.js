76993176f118fd1fdb9dd67f3a5fd6227e745f4d
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-02-25 15:07:27 +0200

    z

--- Frontend/script.js
+++ Frontend/script.js
@@ -10,14 +10,17 @@ class Card {
 
 class Deck {
   constructor() {
-    this.deck = [];
-  }
-
-  fillUp() {
-    let rank = 13;
+    let rank = 14;
     let arr = [];
 
     while (rank) {
+      if (rank === 14) {
+        arr.push(new Card("Heart", "A"));
+        arr.push(new Card("Club", "A"));
+        arr.push(new Card("Diamond", "A"));
+        arr.push(new Card("Spade", "A"));
+        rank--;
+      }
       if (rank === 13) {
         arr.push(new Card("Heart", "K"));
         arr.push(new Card("Club", "K"));
@@ -50,29 +53,34 @@ class Deck {
     }
     this.deck = arr;
   }
+
+  static deal() {
+    const randomCard = tableDeck[Math.floor(Math.random() * tableDeck.length)];
+    this.hand.push(tableDeck[randomCard]);
+    tableDeck.remove(randomCard);
+  }
 }
 
 class PlayerDeck extends Deck {
   constructor() {
     super();
+    this.hand = [];
   }
 }
 
 class TableDeck extends Deck {
   constructor() {
     super();
+    this.table = [];
   }
 }
 
 class PileDeck extends Deck {
   constructor() {
     super();
+    this.pile = [];
   }
 }
-
-const deck = document.getElementById("deck");
-
-deck.addEventListener("onclick", dealDeck());
 
 function dealDeck() {}
 
@@ -80,8 +88,19 @@ function dealDeck() {}
 
 // console.log(cardDeck.log);
 
-const newDeck = new Deck();
-
-newDeck.fillUp();
+let newDeck = new Deck();
 
 console.log(newDeck.deck);
+
+let player1Deck = new PlayerDeck();
+let player2Deck = new PlayerDeck();
+let player3Deck = new PlayerDeck();
+let player4Deck = new PlayerDeck();
+
+let table = new tableDeck();
+
+let pile = new PileDeck();
+
+const deck = document.getElementById("deck");
+
+deck.addEventListener("onclick", dealDeck());
